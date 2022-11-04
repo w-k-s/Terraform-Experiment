0de82ecf6b3332d21b69f28ce6e004327e53430a
w-k-s <waqqas.abdulkareem@gmail.com>
2022-11-04 17:32:16 +0400

    Fix terraform pipeline

--- StaticWebsite/terraform/main.tf
+++ StaticWebsite/terraform/main.tf
@@ -1,1 +1,31 @@
-main.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  backend "s3" {
+    # This assumes we have a bucket created called io.wks.terraform
+    bucket = "io.wks.terraform"
+    key    = "static-website.state.json"
+    region = "ap-south-1"
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "terraform.wks.io"
+  acl    = "public-read"
+
+  aws_s3_bucket_website_configuration = {
+    index_document = "index.html"
+  }
+}
