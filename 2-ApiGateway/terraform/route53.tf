5b2753d1fbb18c9f64a99070e3cf231a368ddbdb
w-k-s <waqqas.abdulkareem@gmail.com>
2022-11-29 13:55:11 +0400

    Fixed formatting

--- 2-ApiGateway/terraform/route53.tf
+++ 2-ApiGateway/terraform/route53.tf
@@ -4,8 +4,8 @@ resource "aws_route53_record" "root-a" {
   type    = "A"
 
   alias {
-    name    = aws_api_gateway_domain_name.api_domain.cloudfront_domain_name
-    zone_id = aws_api_gateway_domain_name.api_domain.cloudfront_zone_id
+    name                   = aws_api_gateway_domain_name.api_domain.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.api_domain.cloudfront_zone_id
     evaluate_target_health = false
   }
 }
