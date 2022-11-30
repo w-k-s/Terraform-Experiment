ec0bd498952cffc22a1b1b7df265509aefb63e05
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2022-11-30 15:12:41 +0400

    Remove default arguments

--- 2-ApiGateway/terraform/variables.tf
+++ 2-ApiGateway/terraform/variables.tf
@@ -6,5 +6,4 @@ variable "hosted_zone_name" {
 variable "api_gateway_host" {
   type        = string
   description = "The url host of the api e.g. api.example.com"
-  default     = format("api.%s", var.hosted_zone_name)
 }
