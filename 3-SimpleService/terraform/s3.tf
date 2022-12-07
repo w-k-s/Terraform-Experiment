ce68f763439f5aab86442b4c15b0a3cecb641353
w-k-s <waqqas.abdulkareem@gmail.com>
2022-12-07 22:29:12 +0400

    Complete first draft of Simple Service terraform manifest.

--- 3-SimpleService/terraform/s3.tf
+++ 3-SimpleService/terraform/s3.tf
@@ -2,3 +2,11 @@ resource "aws_s3_bucket" "app_bucket" {
   bucket = "io.wks.terraform.todobackend"
   acl    = "private"
 }
+
+resource "aws_s3_object" "app" {
+  bucket = aws_s3_bucket.app_bucket.id
+  key    = "app.jar"
+  acl    = "private"
+  source = var.executable_jar_path
+  etag   = filemd5(var.executable_jar_path)
+}
--- 3-SimpleService/terraform/variables.tf
+++ 3-SimpleService/terraform/variables.tf
@@ -1,3 +1,9 @@
+variable "project_name" {
+  type        = string
+  description = "the name of the project"
+  default     = "Simple Service"
+}
+
 variable "hosted_zone_name" {
   type        = string
   description = "The hosted zone domain name. For example, the hosted_zone_name for 'http://api.example.org' is 'example.org'"
@@ -12,3 +18,15 @@ variable "executable_jar_path" {
   type        = string
   description = "The absolute path to the executable jar" # Can this be relative? 
 }
+
+variable "ec2_key_pair_name" {
+  type        = string
+  description = "The name used for the EC2 instance(s) key pair"
+  default     = "terraform_simple_service_key_pair"
+}
+
+variable "ec2_key_pair_public_key_content" {
+  type        = string
+  description = "The content of the public key used as the ec2 instance key pair"
+}
+
