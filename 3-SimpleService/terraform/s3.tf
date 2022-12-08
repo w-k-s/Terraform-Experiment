8e0841afe97ec31b6c773f3c7879e92495b66381
w-k-s <waqqas.abdulkareem@gmail.com>
2022-12-08 11:23:40 +0400

    Fix issue of deleting s3 bucket

--- 3-SimpleService/terraform/s3.tf
+++ 3-SimpleService/terraform/s3.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "app_bucket" {
-  bucket = "io.wks.terraform.todobackend"
+  bucket        = "io.wks.terraform.todobackend"
+  force_destroy = true
 }
 
-resource "aws_s3_bucket_acl" "app_bucket_acl"{
+resource "aws_s3_bucket_acl" "app_bucket_acl" {
   bucket = aws_s3_bucket.app_bucket.id
   acl    = "private"
 }
@@ -12,5 +13,5 @@ resource "aws_s3_object" "app" {
   key    = "app.jar"
   acl    = "private"
   source = var.executable_jar_path
-  etag   = filemd5(var.executable_jar_path)
+  //etag   = filemd5(var.executable_jar_path) // TODO: File doesn't exist when destroying.
 }
