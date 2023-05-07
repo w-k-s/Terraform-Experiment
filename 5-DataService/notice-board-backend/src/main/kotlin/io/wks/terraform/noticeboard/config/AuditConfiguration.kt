6a25b2105d76e68453233110c133f128ecdc584c
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2023-05-07 15:10:09 +0400

    Fix issue: DB can't be created in TF due to private subnet

--- 5-DataService/notice-board-backend/src/main/kotlin/io/wks/terraform/noticeboard/config/AuditConfiguration.kt
+++ 5-DataService/notice-board-backend/src/main/kotlin/io/wks/terraform/noticeboard/config/AuditConfiguration.kt
@@ -5,12 +5,11 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.data.domain.AuditorAware
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import java.util.*
 
 class JwtAuditorAware : AuditorAware<String> {
     override fun getCurrentAuditor(): Optional<String> =
-        Optional.ofNullable((SecurityContextHolder.getContext().authentication as? JwtAuthenticationToken)?.name)
+        Optional.ofNullable((SecurityContextHolder.getContext().authentication?.name))
 
 }
 
--- 5-DataService/terraform/ssm.tf
+++ 5-DataService/terraform/ssm.tf
@@ -11,49 +11,56 @@ resource "aws_ssm_parameter" "cloudwatch_agent_config" {
 
 resource "aws_ssm_parameter" "cognito_client_id" {
   description = "Cognito Client ID"
-  name        = format("/%s/oauth2/client_id", var.project_id)
+  name        = format("/config/%s/spring.security.oauth2.client.registration.cognito.clientId", var.project_id)
   type        = "String"
   value       = aws_cognito_user_pool_client.this.id
 }
 
 resource "aws_ssm_parameter" "cognito_client_secret" {
-  description = "Cognito Client ID"
-  name        = format("/%s/oauth2/client_secret", var.project_id)
+  description = "Cognito Client Secret"
+  name        = format("/config/%s/spring.security.oauth2.client.registration.cognito.clientSecret", var.project_id)
   type        = "SecureString"
   value       = aws_cognito_user_pool_client.this.client_secret
 }
 
-resource "aws_ssm_parameter" "application_db_username" {
+resource "aws_ssm_parameter" "cognito_client_redirect_uri" {
+  description = "Cognito Client Scope"
+  name        = format("/config/%s/spring.security.oauth2.client.registration.cognito.redirect-uri", "openid")
+  type        = "String"
+  value       = "http://localhost:8080/login/oauth2/code/cognito"
+}
+
+resource "aws_ssm_parameter" "cognito_client_name" {
+  description = "Cognito Client Scope"
+  name        = format("/config/%s/spring.security.oauth2.client.registration.cognito.clientName", "openid")
+  type        = "String"
+  value       = aws_cognito_user_pool_client.this.name
+}
+
+resource "aws_ssm_parameter" "cognito_client_issuer_uri" {
+  description = "Cognito Client Scope"
+  name        = format("/config/%s/spring.security.oauth2.client.provider.issuerUri", "openid")
+  type        = "String"
+  value       = format("https://cognito-idp.%s.amazonaws.com/%s", var.aws_region, aws_cognito_user_pool.this.id)
+}
+
+resource "aws_ssm_parameter" "application_jdbc_url" {
   description = "The username that the application uses to connect to the DB"
-  name        = format("/%s/db/username", var.project_id)
+  name        = format("/config/%s/spring.datasource.url", var.project_id)
+  type        = "SecureString"
+  value       = format("jdbc:postgresql://%s:%s/%s?currentSchema=%s", data.aws_db_instance.database.host, data.aws_db_instance.database.port, var.rds_psql_application_db_name, var.rds_psql_application_schema)
+}
+
+resource "aws_ssm_parameter" "application_db_username" {
+  description = "The password that the application uses to connect to the DB"
+  name        = format("/%s/db/spring.datasource.username", var.project_id)
   type        = "SecureString"
   value       = var.rds_psql_application_role
 }
 
 resource "aws_ssm_parameter" "application_db_password" {
-  description = "The password that the application uses to connect to the DB"
-  name        = format("/%s/db/password", var.project_id)
+  description = "The host of the DB that the application connects to"
+  name        = format("/%s/db/spring.datasource.password", var.project_id)
   type        = "SecureString"
   value       = var.rds_psql_application_password
-}
-
-resource "aws_ssm_parameter" "application_db_host" {
-  description = "The host of the DB that the application connects to"
-  name        = format("/%s/db/host", var.project_id)
-  type        = "String"
-  value       = data.aws_db_instance.database.address
-}
-
-resource "aws_ssm_parameter" "application_db_port" {
-  description = "The port of the DB that the application connects to"
-  name        = format("/%s/db/port", var.project_id)
-  type        = "String"
-  value       = data.aws_db_instance.database.port
-}
-
-resource "aws_ssm_parameter" "application_db_schema" {
-  description = "The schema of the DB that the application connects to"
-  name        = format("/%s/db/schema", var.project_id)
-  type        = "String"
-  value       = var.rds_psql_application_schema
 }
