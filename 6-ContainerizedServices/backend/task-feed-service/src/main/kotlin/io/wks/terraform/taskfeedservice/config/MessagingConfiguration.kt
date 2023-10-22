a96e34b0417a2b5207806725e2de23ccd06f6ce5
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2023-10-22 22:06:16 +0400

    Fix issue: Spring Boot 3.1.1 not compatible with Spring Cloud
    (See https://github.com/spring-cloud/spring-cloud-release/issues/266 for details)

--- 6-ContainerizedServices/backend/task-feed-service/src/main/kotlin/io/wks/terraform/taskfeedservice/config/MessagingConfiguration.kt
+++ 6-ContainerizedServices/backend/task-feed-service/src/main/kotlin/io/wks/terraform/taskfeedservice/config/MessagingConfiguration.kt
@@ -1,53 +1,21 @@
 package io.wks.terraform.taskfeedservice.config
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
-import com.amazonaws.regions.Regions
-import com.amazonaws.services.sqs.AmazonSQSAsync
-import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
-import com.fasterxml.jackson.databind.ObjectMapper
-import io.awspring.cloud.messaging.config.QueueMessageHandlerFactory
-import io.awspring.cloud.messaging.core.QueueMessagingTemplate
+import io.awspring.cloud.sqs.listener.errorhandler.ErrorHandler
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.messaging.converter.MappingJackson2MessageConverter
-import org.springframework.messaging.handler.annotation.support.PayloadMethodArgumentResolver
-import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver
+import org.springframework.messaging.Message
 
 
 @Configuration
 class MessagingConfiguration {
 
-    @Bean
-    fun queueMessagingTemplate(
-        amazonSQSAsync: AmazonSQSAsync?
-    ): QueueMessagingTemplate? {
-        return QueueMessagingTemplate(amazonSQSAsync)
-    }
+    private val logger = LoggerFactory.getLogger(MessagingConfiguration::class.java)
 
     @Bean
-    fun queueMessageHandlerFactory(
-        mapper: ObjectMapper, amazonSQSAsync: AmazonSQSAsync?
-    ): QueueMessageHandlerFactory? {
-        val queueHandlerFactory = QueueMessageHandlerFactory()
-        queueHandlerFactory.setAmazonSqs(amazonSQSAsync)
-        queueHandlerFactory.setArgumentResolvers(
-            mutableListOf<HandlerMethodArgumentResolver>(
-                PayloadMethodArgumentResolver(jackson2MessageConverter(mapper))
-            )
-        )
-        return queueHandlerFactory
-    }
-
-    private fun jackson2MessageConverter(mapper: ObjectMapper) = MappingJackson2MessageConverter().also {
-        it.objectMapper = mapper
-    }
-
-    @Bean
-    fun amazonSQSAsync(): AmazonSQSAsync? {
-        return AmazonSQSAsyncClientBuilder
-            .standard()
-            .withRegion(Regions.AP_SOUTH_1)
-            .withCredentials(DefaultAWSCredentialsProviderChain())
-            .build()
+    fun errorHandler(): ErrorHandler<Object> = object: ErrorHandler<Object>{
+        override fun handle(message: Message<Object>, t: Throwable) {
+            logger.info("Error processing message: "+message, t)
+        }
     }
 }
--- 6-ContainerizedServices/backend/task-feed-service/src/main/kotlin/io/wks/terraform/taskfeedservice/core/messaging/service/MessageReceiver.kt
+++ 6-ContainerizedServices/backend/task-feed-service/src/main/kotlin/io/wks/terraform/taskfeedservice/core/messaging/service/MessageReceiver.kt
@@ -1,10 +1,9 @@
 package io.wks.terraform.taskfeedservice.core.messaging.service
 
-import io.awspring.cloud.messaging.listener.annotation.SqsListener
+import io.awspring.cloud.sqs.annotation.SqsListener
 import io.wks.terraform.taskfeedservice.core.feed.service.TaskFeedService
 import io.wks.terraform.taskfeedservice.core.messaging.NewTaskMessage
 import org.slf4j.LoggerFactory
-import org.springframework.messaging.handler.annotation.Header
 import org.springframework.stereotype.Service
 
 @Service
@@ -15,11 +14,10 @@ class MessageReceiver(
     private val LOGGER = LoggerFactory.getLogger(MessageReceiver::class.java)
 
     @SqsListener(
-        value = ["\${messaging.queue.tasks.name}"],
+        value = ["\${messaging.queue.tasks.name}"]
     )
     fun receiveMessage(
-        message: NewTaskMessage,
-        @Header("SenderId") senderId: String?
+        message: NewTaskMessage
     ) {
         LOGGER.info("Message Received: '$message'")
         taskFeedService.onNewTask(message)
