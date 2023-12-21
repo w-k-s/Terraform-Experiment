2126ec362efef139a8ff5ce9bd42e76a2a2b4ea6
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2023-12-21 20:37:54 +0400

    Variable name

--- 7-OrchestratedServices/terraform/variables.tf
+++ 7-OrchestratedServices/terraform/variables.tf
@@ -10,7 +10,7 @@ variable "project_id" {
   default     = "shop"
 }
 
-variable "aws_region" {
+variable "region" {
   type        = string
   description = "The AWS region where the AWS resources will be deployed."
   default     = "ap-south-1"
